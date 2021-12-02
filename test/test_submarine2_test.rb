d79f19905d21cebd1e1a3fafda151e0c645fa1d4
Krzysztof Urban <krzysztof.urban@netguru.pl>
2021-12-02 15:15:19 +0100

    add submarine v2

---
+++ test/test_submarine2_test.rb
@@ -0,0 +1,62 @@
+require 'minitest/autorun'
+
+require './lib/submarine2'
+
+class TestSubmarine2 < Minitest::Test
+  def setup
+    @submarine = Submarine2.new
+    @initial_state = {
+      horizontal: 0,
+      depth: 0,
+      multiplied: 0,
+      aim: 0,
+    }
+  end
+
+  def test_that_it_ends_up_in_right_place
+    commands = [
+      [:forward, 5],
+      [:down, 5],
+      [:forward, 8],
+      [:up, 3],
+      [:down, 8],
+      [:forward, 2],
+    ]
+
+    state = @submarine.run(@initial_state, commands)
+
+    assert_equal({
+      horizontal: 15,
+      depth: 60,
+      aim: 10,
+      multiplied: 900,
+    }, state)
+  end
+
+  def test_forward
+    state = @submarine.next(@initial_state, [:forward, 5])
+    assert_equal 5, state[:horizontal]
+  end
+
+  def test_forward_upwards
+    state = @submarine.next(@initial_state.merge(aim: -5), [:forward, 5])
+    assert_equal 5, state[:horizontal]
+    assert_equal -25, state[:depth]
+  end
+
+  def test_forward_downwards
+    state = @submarine.next(@initial_state.merge(aim: 5), [:forward, 5])
+    assert_equal 5, state[:horizontal]
+    assert_equal 25, state[:depth]
+  end
+
+  def test_up
+    state = @submarine.next(@initial_state, [:up, 5])
+    assert_equal -5, state[:aim]
+  end
+
+  def test_down
+    state = @submarine.next(@initial_state, [:down, 5])
+    assert_equal 5, state[:aim]
+  end
+end
