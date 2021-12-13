2d1189032f7d46d940f9c4a12ae18f13c7f2199b
Krzysztof Urban <krzysztof.urban@netguru.pl>
2021-12-13 17:11:36 +0100

    day04 both solutions

--- lib/bingo.rb
+++ lib/bingo.rb
@@ -1,26 +1,102 @@
 class Bingo
-  class Result
+  class Board
+    attr_reader :array
+
+    def initialize(array)
+      @array = array
+      @rows = array.each_slice(5).to_a
+      @columns = @rows.transpose
+    end
+
+    def winning?(called)
+      (@rows + @columns).each do |line|
+        return true if winning_line?(line, called)
+      end
+
+      false
+    end
+
+    def ==(other)
+      array == other.array
+    end
+
+    private
+
+    def winning_line?(line, called)
+      line.all? { |x| called.include?(x) }
+    end
+  end
+
+  class Win
+    attr_reader :board, :called
+
+    def initialize(board, called)
+      @board = board
+      @called = called
+    end
+
     def sum_of_unmarked_numbers
-      188
+      @board.array.reject { |x| @called.include?(x) }.sum
     end
 
     def winning_number
-      24
+      @called.last
     end
 
     def final_score
       sum_of_unmarked_numbers * winning_number
     end
 
-    def board_idx
-      2
+    def ==(other)
+      !other.nil? && @board == other.board && @called == other.called
     end
   end
 
-  def initialize(numbers:, boards:)
+  attr_reader :boards, :numbers
+
+  def self.load(data, numbers)
+    boards = data.split("\n\n").map do |chunk|
+      array = chunk.split(" ").map(&:to_i)
+      Bingo::Board.new(array)
+    end
+
+    new(numbers: numbers, boards: boards)
   end
 
-  def run
-    Result.new
+  def initialize(numbers:, boards:)
+    @numbers = numbers
+    @boards = boards
+  end
+
+  def win
+    to_call = @numbers.dup
+    called = []
+
+    while to_call.length > 0
+      called << to_call.shift
+      winning_board = @boards.find { |board| board.winning?(called) }
+
+      return Win.new(winning_board, called) unless winning_board.nil?
+    end
+
+    nil
+  end
+
+  def lose
+    to_call = @numbers.dup
+    called = []
+    remaining_boards = @boards.dup
+
+    while to_call.length > 0
+      called << to_call.shift
+
+      if remaining_boards.length == 1 && remaining_boards.first.winning?(called)
+        return Win.new(remaining_boards.first, called)
+      end
+
+      remaining_boards = remaining_boards.reject { |board| board.winning?(called) }
+    end
+
+    nil
   end
 end
--- test/test_bingo_test.rb
+++ test/test_bingo_test.rb
@@ -6,7 +6,7 @@ class TestBingo < Minitest::Test
   def setup
     @numbers = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8,
                 19, 3, 26, 1]
-    @boards = <<~DATA
+    @data = <<~DATA
       22 13 17 11  0
        8  2 23  4 24
       21  9 14 16  7
@@ -26,26 +26,68 @@ class TestBingo < Minitest::Test
        2  0 12  3  7
     DATA
 
-    @bingo = Bingo.new(numbers: @numbers, boards: @boards)
+    @bingo = Bingo.load(@data, @numbers)
+    @board = Bingo::Board.new(
+      [
+        14, 21, 17, 24,  4,
+        10, 16, 15,  9, 19,
+        18,  8, 23, 26, 20,
+        22, 11, 13,  6,  5,
+         2,  0, 12,  3,  7,
+      ],
+    )
+    @losing_board = Bingo::Board.new(
+      [
+         3, 15,  0,  2, 22,
+         9, 18, 13, 17,  5,
+        19,  8,  7, 25, 23,
+        20, 11, 10, 24,  4,
+        14, 21, 16, 12,  6,
+      ],
+    )
+    @win = Bingo::Win.new(@board, [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24])
+    @lose = Bingo::Win.new(@losing_board, [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13])
+  end
+
+  def test_board_loading
+    bingo = Bingo.load(@data, @numbers)
+    assert_equal @numbers, bingo.numbers
+    assert_equal @board, bingo.boards[2]
   end
 
   def test_sum_of_unmarked_numbers
-    result = @bingo.run
-    assert_equal 188, result.sum_of_unmarked_numbers
+    assert_equal 188, @win.sum_of_unmarked_numbers
   end
 
   def test_winning_number
-    result = @bingo.run
-    assert_equal 24, result.winning_number
+    assert_equal 24, @win.winning_number
   end
 
   def test_final_score
-    result = @bingo.run
-    assert_equal 4512, result.final_score
+    assert_equal 4512, @win.final_score
   end
 
-  def test_board_idx
-    result = @bingo.run
-    assert_equal 2, result.board_idx
+  def test_bingo_board_doesnt_win_with_no_calls
+    assert_equal false, @board.winning?([])
+  end
+
+  def test_bingo_board_doesnt_win
+    assert_equal false, @board.winning?([1, 2, 3, 4, 5])
+  end
+
+  def test_bingo_board_wins_horizontal
+    assert_equal true, @board.winning?([18, 8, 23, 26, 20])
+  end
+
+  def test_bingo_board_wins_vertical
+    assert_equal true, @board.winning?([17, 15, 23, 13, 12])
+  end
+
+  def test_bingo_finds_the_winning_board
+    assert_equal @win, @bingo.win
+  end
+
+  def test_bingo_finds_the_losing_board
+    assert_equal @lose, @bingo.lose
   end
 end
